c53fc3cdb489bca12f1e9c03fce377728c7151ce
jose-robertooliveira <negroafrolife@gmail.com>
2021-09-26 22:25:40 -0300

    project

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function mostra(id) {
+  var display = document.getElementById(id).style.display;
+  if(display == 'none') {
+    document.getElementById(id).style.display = "block";
+  }else {
+    document.getElementById(id).style.display == 'none'; 
+  }
+}
